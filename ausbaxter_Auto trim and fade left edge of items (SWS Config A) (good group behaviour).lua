8c805d592751829bd601103f5b5c3d15e64a014a
Austin Baxter <ausbaxtermp@gmail.com>
2019-07-06 19:41:25 -0700

    Auto trim and fade now receives from SWS command parameters

---
+++ ausbaxter_Auto trim and fade left edge of items (SWS Config A) (good group behaviour).lua
@@ -0,0 +1,72 @@
+--external library usage from Lokasenna, thanks!-----------------------------------
+local info = debug.getinfo(1,'S');
+script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
+
+local function req(file)
+    if missing_lib then return function () end end
+    local ret, err = loadfile(script_path .. file)
+    if not ret then
+        reaper.ShowMessageBox("Couldn't load "..file.."\n\nError: "..tostring(err), "Library error", 0)
+        missing_lib = true    
+        return function () end
+    else 
+        return ret
+    end  
+end
+------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------------
+reaper_path = reaper.GetResourcePath() .. "/Xenakios_Commands.ini"
+if not reaper.file_exists(reaper_path) then 
+    reaper.ShowMessageBox("SWS Extension is required for this script.", "SWS Not Installed", 0)
+    return
+end
+retval, ini_fade_in = reaper.BR_Win32_GetPrivateProfileString("XENAKIOSCOMMANDS", "FADEINTIMEA", "1.0", reaper_path)
+local fade_length = tonumber(ini_fade_in) * 1000
+------------------------------------------------------------------------------------
+
+--library requirement
+req("ausbaxter_Helper_functions.lua")()--remove at some point
+
+function LeftTrim(fade_length)
+
+    if reaper.CountSelectedMediaItems(0) > 0 then
+
+        sel_items = GetAllSelectedItemsInTable()
+        cursor = reaper.GetCursorPosition()
+
+        reaper.PreventUIRefresh(1)
+        reaper.Undo_BeginBlock()
+
+        local grp_state = reaper.GetToggleCommandState(1156) --group override
+        if grp_state == 1 then reaper.Main_OnCommand(1156, 0) end --ungroup
+
+        reaper.Main_OnCommand(40289,0)--unselect items
+
+        for i, item in ipairs(sel_items) do
+            local i_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+            local i_end = i_pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+            if i_pos < cursor and i_end > cursor then
+                reaper.SetMediaItemInfo_Value(item, "B_UISEL", 1)
+                reaper.ApplyNudge(0, 1, 1, 0, cursor-fade_length, false, 0)
+                reaper.SetMediaItemInfo_Value(item, "D_FADEINLEN", fade_length)
+                reaper.SetMediaItemInfo_Value(item, "B_UISEL", 0)
+            end
+        end
+
+        if grp_state == 1 then reaper.Main_OnCommand(1156, 0) end --regroup
+
+        for i, item in ipairs(sel_items) do reaper.SetMediaItemInfo_Value(item, "B_UISEL", 1) end
+
+        reaper.Undo_EndBlock("Trim left edge", -1)
+        reaper.UpdateArrange()
+
+    end
+
+end
+
+function main()
+    LeftTrim(fade_length/1000)
+end
+
+main()
