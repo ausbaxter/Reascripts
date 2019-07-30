476e86b63706cc873412b0247cbaa7808f8de969
Austin Baxter <ausbaxtermp@gmail.com>
2019-07-30 08:58:54 -0700

    Improvements to smart edit functions (delete, duplicate)

--- ausbaxter_Smart delete.lua
+++ ausbaxter_Smart delete.lua
@@ -1,70 +1,95 @@
-function Print(value)
-	reaper.ShowConsoleMsg(value)
+-- TODO if no items are selected still apply crossfading to resulting edits if ripple editing enabled
+
+function SaveSelectedItems()
+
+	media_items = {}
+
+	for i = 0, reaper.CountSelectedMediaItems(0) do
+		table.insert(media_items, reaper.GetSelectedMediaItem(0, i))
+	end
+
+	return media_items
+
 end
 
-function GetPostEditItem(tSStart)
+function RestoreSelectedItems(media_items)
+	for i, item in ipairs(media_items) do
+		reaper.SetMediaItemInfo_Value(item, "B_UISEL", 1)
+	end
+end
 
-	local first_item
-	local second_item
+function IsItemContiguous(first_item, second_item)
 
-	for i = 0, reaper.CountSelectedTracks(0) - 1 do
-		
-		local track = reaper.GetSelectedTrack(0, i)
-		
-		for j = 0, reaper.CountTrackMediaItems(track) - 1 do
+	local fi_track = reaper.GetMediaItem_Track(first_item)
+	local si_track = reaper.GetMediaItem_Track(second_item)
 
-			local item = reaper.GetTrackMediaItem(track, j)
-			local item_start = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-			local item_end = item_start + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+	local fi_end = reaper.GetMediaItemInfo_Value(first_item, "D_POSITION") + reaper.GetMediaItemInfo_Value(first_item, "D_LENGTH")
+	local si_start = reaper.GetMediaItemInfo_Value(first_item, "D_POSITION")
 
-			if tSStart == item_end then 
-				first_item = item
-				second_item = reaper.GetTrackMediaItem(track, j + 1)
-				return first_item, second_item
-			end
+	if fi_end >= si_start and fi_track == si_track then return true end
+
+	return false
+
+end
+
+function CrossfadeRippledItems(media_items)
+
+	local fade_len = 0.02
+
+	for i = 2, #media_items do
+
+		local earlier_item = media_items[i-1]
+		local this_item = media_items[i]
+
+		if IsItemContiguous(earlier_item, this_item) then
+
+			reaper.SetMediaItemInfo_Value(earlier_item, "D_FADEOUTLEN_AUTO", fade_len)
+			reaper.SetMediaItemInfo_Value(earlier_item, "C_FADEOUTSHAPE", 1)
+
+			reaper.SetMediaItemInfo_Value(this_item, "B_UISEL", 1)
+			reaper.ApplyNudge(0, 0, 1, 1, -fade_len, false, 0)
+			reaper.SetMediaItemInfo_Value(this_item, "B_UISEL", 0)
+
+			reaper.SetMediaItemInfo_Value(this_item, "D_FADEINLEN_AUTO", fade_len)
+			reaper.SetMediaItemInfo_Value(this_item, "C_FADEINSHAPE", 1)
+
+		end
+	
+	end
+
+end
+
+function Main()
+
+	local context = reaper.GetCursorContext()
+	local equal_power = 41529
+	local save_item_sel = reaper.NamedCommandLookup("_SWS_SAVEALLSELITEMS1")
+	local load_item_sel = reaper.NamedCommandLookup("_SWS_RESTALLSELITEMS1")
+	local unselect_items = 40289
+
+	selectedItems = reaper.CountSelectedMediaItems()
+	reaper.Undo_BeginBlock()
+
+	tSStart, tSEnd = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
+
+	if (tSEnd - tSStart) == 0 or context == 0 then
+		reaper.Main_OnCommand(40697, 0)
+
+	else
+		reaper.Main_OnCommand(40312, 0)
+		reaper.Main_OnCommand(40635, 0)
+		if reaper.GetToggleCommandState(41990) == 1 then
+
+			selected_media_items = SaveSelectedItems()
+			reaper.Main_OnCommand(unselect_items, 0)
+			CrossfadeRippledItems(selected_media_items)
+			RestoreSelectedItems(selected_media_items)
 
 		end
 	end
+
+	reaper.Undo_EndBlock("Smart Delete", 0)
+
 end
 
-local context = reaper.GetCursorContext()
-local equal_power = 41529
-local save_item_sel = reaper.NamedCommandLookup("_SWS_SAVEALLSELITEMS1")
-local load_item_sel = reaper.NamedCommandLookup("_SWS_RESTALLSELITEMS1")
-local unselect_items = 40289
-local fade_len = 0.02
-
-selectedItems = reaper.CountSelectedMediaItems()
-reaper.Undo_BeginBlock()
-
-tSStart, tSEnd = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
-  
-if (tSEnd - tSStart) == 0 or context == 0 then -- No TS
-	reaper.Main_OnCommand(40697, 0)
-
-else -- Yes TS
-	reaper.Main_OnCommand(40312, 0)
-	reaper.Main_OnCommand(40635, 0)
-	if reaper.GetToggleCommandState(41990) == 1 then --shuffle edit?
-
-		reaper.Main_OnCommand(save_item_sel, 0)
-		reaper.Main_OnCommand(unselect_items, 0)
-
-		local first_item, second_item = GetPostEditItem(tSStart)
-
-		reaper.SetMediaItemInfo_Value(first_item, "D_FADEOUTLEN_AUTO", fade_len)
-		reaper.SetMediaItemInfo_Value(first_item, "C_FADEOUTSHAPE", 1)
-
-		reaper.SetMediaItemInfo_Value(second_item, "B_UISEL", 1)
-		reaper.ApplyNudge(0, 0, 1, 1, -fade_len, false, 0)
-
-		reaper.SetMediaItemInfo_Value(second_item, "D_FADEINLEN_AUTO", fade_len)
-		reaper.SetMediaItemInfo_Value(second_item, "C_FADEINSHAPE", 1)
-		
-		reaper.Main_OnCommand(load_item_sel, 0)
-
-	end
-end
-
-reaper.Undo_EndBlock("Smart Delete", 0)
-
+Main()
--- ausbaxter_Smart duplicate items.lua
+++ ausbaxter_Smart duplicate items.lua
@@ -1,11 +1,14 @@
-function Print(value)
-  reaper.ShowConsoleMsg(value)
-end
+-- TODO duplicate envelope points if no item selected (smart copy has implementation for looking up envelope points)
 
 selectedItems = reaper.CountSelectedMediaItems()
+disableTrimBehind = 41121
+enableTrimBehind = 41120
+
 reaper.Undo_BeginBlock()
 
 if selectedItems > 0 then
+
+  reaper.Main_OnCommand(enableTrimBehind, 0)
 
   tSStart, tSEnd = 
   reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
@@ -16,8 +19,12 @@ if selectedItems > 0 then
     reaper.Main_OnCommand(41296, 0)
   end
   
+  reaper.Main_OnCommand(disableTrimBehind, 0)
+
   reaper.Undo_EndBlock("Smart Duplicate", 0)
   
 else
+
   reaper.Undo_EndBlock("", 1)
+
 end
